97e783dca5c5914162dc56cdb7cf21f4cb0e424d
ianiketbasu <aniketbasu851595@gmail.com>
2024-03-10 23:22:53 +0530

    register method implemented

--- src/app.js
+++ src/app.js
@@ -11,9 +11,17 @@ app.use(
   })
 );
 
+
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+// routes import
+import userRouter from "./routes/user.routes.js";
+
+// router declaration 
+app.use("/api/v1/users",userRouter);
 
 export { app };
